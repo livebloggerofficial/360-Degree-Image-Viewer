fcbb05140bd5bd2c84c0361dd2691c559cbabe11
Live Blogger <61611286+livebloggerofficial@users.noreply.github.com>
2021-07-20 15:06:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const panoramaImage = new PANOLENS.ImagePanorama("images/image1.jpeg");
+const imageContainer = document.querySelector(".image-container");
+
+const viewer = new PANOLENS.Viewer({
+  container: imageContainer,
+  autoRotate: true,
+  autoRotateSpeed: 0.3,
+  controlBar: false,
+});
+
+viewer.add(panoramaImage);
